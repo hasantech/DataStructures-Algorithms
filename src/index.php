6ca33b9b15a351962e144e261a1c2e28ba75bab6
hasantech <30698449+hasantech@users.noreply.github.com>
2017-09-21 18:00:46 +0300

    Add Project skeleton

---
+++ src/index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Scrolling Nav - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/scrolling-nav.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="index.php">DataStructures & Algorithms</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="complexityanalysis.php">Complexity analysis</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="recursiveDataStructures.php">Recursive DataStructures</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="elementaryDataStructures.php">Elementary DataStructures</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="sortingAlgorithms.php">Sorting Algorithms</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="searchingAlgorithms.php">Searching Algorithms</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="treeAlgorithms.php">Tree algorithms</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="graphAlgorithms.php">Graph Algorithms</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="project.php">Project</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+      <div class="item active">
+        <img src="img/img2.png" alt="Image 1">
+      </div>
+ 
+    </div>
+
+    <div class="row" id="leftcontent">
+      <div class="col-md-2" >
+
+      </div>
+      <div class="col-md-6" style="background-color:#f0f5f5;">
+        
+    <section id="about" style=" padding: 10px;">
+      <div class="container">
+        <div class="row">
+          <div >
+            <h2 style="margin-top: 10px;">Welcome to DataStructures & Angorithms</h2>
+            <p class="lead">This is a great place to talk about your webpage. This template is purposefully unstyled so you can use it as a boilerplate or starting point for you own landing page designs! This template features:</p>
+            <ul>
+              <li>Clickable nav links that smooth scroll to page sections</li>
+              <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+              <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
+              <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+            </ul>
+            <ul>
+              <li>Clickable nav links that smooth scroll to page sections</li>
+              <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+              <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
+              <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+            </ul>
+            <ul>
+              <li>Clickable nav links that smooth scroll to page sections</li>
+              <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+              <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
+              <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+            </ul>
+            <ul>
+              <li>Clickable nav links that smooth scroll to page sections</li>
+              <li>Responsive behavior when clicking nav links perfect for a one page website</li>
+              <li>Bootstrap's scrollspy feature which highlights which section of the page you're on in the navbar</li>
+              <li>Minimal custom CSS so you are free to explore your own unique design options</li>
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </section>
+      </div>
+      <div class="col-md-3">
+      <h3 style="margin-top: 10px;">Register with us for more updates</h3>
+      <div style="box-shadow: 5px 5px 5px #888888; padding:5px; border: 1px solid #888">
+      <form class="form-horizontal" style="margin: 10px;">
+              <div class="form-group">
+                <label class="control-label " for="email">Email:</label>
+                <div >
+      <input type="email" class="form-control" id="email" placeholder="Enter email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label class="control-label " for="pwd">Password:</label>
+    <div > 
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+    </div>
+  </div>
+  <div class="form-group"> 
+    <div class="col-sm-offset-2 ">
+      <div class="checkbox">
+        <label><input type="checkbox"> Remember me</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group"> 
+    <div class="col-sm-offset-2 ">
+      <button type="submit" class="btn btn-default">Submit</button>
+    </div>
+  </div>
+  </div>
+  </form>
+  
+      </div>
+    </div>
+    </div>
+    
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; DataStructures  2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom JavaScript for this theme -->
+    <script src="js/scrolling-nav.js"></script>
+
+  </body>
+
+</html>
